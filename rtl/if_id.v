5df723180ae968ad137b88c950fcfb3966a828ee
boniface-f <48821641+boniface-f@users.noreply.github.com>
2023-04-19 00:09:57 +0800

    添加功能

--- rtl/if_id.v
+++ rtl/if_id.v
@@ -1,19 +1,23 @@
-`include "../rtl/defines.v"
+`include "../rtl/defines.v" 
 
 module if_id(
 	input wire clk,
 	input wire rst,
+	input wire hold_flag_i,
 	input wire [31:0]  inst_i, 
 	input wire [31:0]  inst_addr_i,
 	output wire[31:0]  inst_addr_o, 
 	output wire[31:0]  inst_o
 );
 
-	if_id_dff_set #(32) dff11(clk,rst,`INST_NOP,inst_i,inst_o);
+	if_id_dff_set #(32) dff1(clk,rst,hold_flag_i,`INST_NOP,inst_i,inst_o);
 	
-	if_id_dff_set #(32) dff21(clk,rst,32'b0,inst_addr_i,inst_addr_o);
+	if_id_dff_set #(32) dff2(clk,rst,hold_flag_i,32'b0,inst_addr_i,inst_addr_o);
+
+
 
 endmodule
+
 
 module if_id_dff_set #(
 	parameter DW  = 32
@@ -21,12 +25,13 @@ module if_id_dff_set #(
 (
 	input wire clk,
 	input wire rst,
+	input wire hold_flag_i,
 	input wire [DW-1:0]  set_data, 
 	input wire [DW-1:0]  data_i, 
 	output reg [DW-1:0]  data_o	
 );
 	always @(posedge clk)begin
-		if(rst == 1'b0)
+		if(rst == 1'b0 || hold_flag_i == 1'b1)
 			data_o <= set_data;
 		else
 			data_o <= data_i;
--- rtl/rom.v
+++ rtl/rom.v
@@ -3,7 +3,7 @@ module rom(
 	output reg[31:0] inst_o
 );
 
-	reg[31:0] rom_mem[0:4095];  //4096 个 32b的 空间
+	reg[31:0] rom_mem[0:4095];  //4096 个 32b的 空间 
 	
 	always @(*)begin
 		inst_o = rom_mem[inst_addr_i>>2];
